4eabdd24d4134f944ee8e046b173deb1711f3960
Rodrigo <rodsussumu@gmail.com>
2025-08-24 22:46:35 -0300

    chore: change cors url

--- src/main/java/com/rodsussumu/riachuelo_backend/application/config/SecurityConfig.java
+++ src/main/java/com/rodsussumu/riachuelo_backend/application/config/SecurityConfig.java
@@ -71,7 +71,7 @@ public class SecurityConfig {
         cfg.setAllowedOrigins(List.of(
                 "http://localhost:4200",
                 "http://127.0.0.1:4200",
-                "https://seu-front-em-producao.com"
+                "https://riachuelo-challenge-frontend.onrender.com"
         ));
 
         cfg.setAllowedMethods(List.of("GET","POST","PUT","PATCH","DELETE","OPTIONS"));
